2af3845677191b321f917df4c26df4da1c10554f
Stanislav <S@mail.ru>
2022-12-10 13:33:36 +0300

    add Task 2

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -1,12 +1,12 @@
 ﻿// Программа принимает на входе пятизначное число и проверяет, является ли число палиндромом.
 Console.WriteLine("Введите пятизначное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
-int palindrome, sum = 0;
+int digit, sum = 0;
 int temp = num;
 while (num > 0)
 {
-    palindrome = num % 10;
-    sum = (sum * 10) + palindrome;
+    digit = num % 10;
+    sum = (sum * 10) + digit;
     num = num / 10;
 }
 if (temp == sum)
