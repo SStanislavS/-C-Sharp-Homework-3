56e18a0a1b41e666367dbeb4409045e16b77da23
Stanislav <S@mail.ru>
2022-12-13 15:31:16 +0300

    add Task 3

---
+++ Task 3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Программа принимает на входе число (N) и помещает в массив таблицу кубов чисел от 1 до N.
+// Console.WriteLine("Введите число N: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// 
+// {
+//     for (int i = 1; i <= N; i++)
+//     Console.Write($"{Math.Pow(i, 3)}, ");
+// }
+// Console.WriteLine("Cube: " + N);
+
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+
+int[] array = new int [N];
+
+Console.Write("[");
+
+for (int i = 1; i <= N; i++)
+{
+    Console.Write(i * i * i + ",");
+}
+Console.Write("]");
